53aab6adcf3a04e0b9bff52ac8b04de1af5afcdc
saranpong <creadz@gmail.com>
2021-08-16 18:35:57 +0700

    update code

--- server.js
+++
@@ -1,7 +0,0 @@
-const openssl = require('openssl-nodejs')
-
-openssl(['req', '-config', 'csr.conf', '-out', 'CSR.csr', '-new', '-newkey', 'rsa:2048', '-nodes', '-keyout', 'privateKey.key']
-   , function (err, buffer) {
-      console.log(err.toString(), buffer.toString());
-   });
-
